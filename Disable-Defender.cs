abfe54c252a588e513a98b95b739413e970eff8d
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-03-11 16:16:54 +0200

    Add files via upload

---
+++ Disable-Defender.cs
@@ -0,0 +1,46 @@
+using Microsoft.Win32;
+using System.Windows.Forms;
+
+class Program
+{
+    static void Main()
+    {
+        SetDword(RegistryHive.LocalMachine, @"SOFTWARE\Policies\Microsoft\Windows Defender", "ServiceKeepAlive", 1);
+        SetDword(RegistryHive.LocalMachine, @"SOFTWARE\Policies\Microsoft\Windows Defender", "DisableAntiSpyware", 1);
+        SetDword(RegistryHive.LocalMachine, @"SOFTWARE\Policies\Microsoft\Windows Defender", "DisableAntiVirus", 1);
+
+        SetDword(RegistryHive.LocalMachine, @"SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection", "DisableRealtimeMonitoring", 1);
+
+        SetDword(RegistryHive.LocalMachine, @"SOFTWARE\Policies\Microsoft\Windows Defender\Spynet", "SpynetReporting", 0);
+        SetDword(RegistryHive.LocalMachine, @"SOFTWARE\Policies\Microsoft\Windows Defender\Spynet", "SubmitSamplesConsent", 2);
+
+        SetDword(RegistryHive.CurrentUser, @"Software\Microsoft\Windows\CurrentVersion\AppHost", "EnableWebContentEvaluation", 0);
+
+        SetDword(RegistryHive.LocalMachine, @"SYSTEM\CurrentControlSet\Control\CI\Policy", "VerifiedAndReputablePolicyState", 0);
+
+        SetDword(RegistryHive.LocalMachine, @"SOFTWARE\Policies\Microsoft\Windows\System", "EnableSmartScreen", 0);
+
+        SetDword(RegistryHive.LocalMachine, @"SYSTEM\CurrentControlSet\Control\Lsa", "RunAsPPL", 0);
+
+        SetDword(RegistryHive.LocalMachine, @"SOFTWARE\Policies\Microsoft\Windows\WTDS\Components", "ServiceEnabled", 0);
+
+        SetDword(RegistryHive.LocalMachine, @"SOFTWARE\Policies\Microsoft\Edge", "SmartScreenEnabled", 0);
+
+        SetDword(RegistryHive.LocalMachine, @"SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity", "Enabled", 0);
+
+        SetDword(RegistryHive.LocalMachine, @"SYSTEM\CurrentControlSet\Control\CI\Config", "VulnerableDriverBlocklistEnable", 0);
+
+        SetDword(RegistryHive.LocalMachine, @"SOFTWARE\Policies\Microsoft\Windows Defender Security Center\App and Browser protection", "DisallowExploitProtectionOverride", 1);
+
+        MessageBox.Show("Windows Defender was permanently disabled. Restart your computer for the changes to take full effect.", "Done");
+    }
+
+    static void SetDword(RegistryHive hive, string path, string name, int value)
+    {
+        using (var baseKey = RegistryKey.OpenBaseKey(hive, RegistryView.Registry64))
+        using (var key = baseKey.CreateSubKey(path))
+        {
+            key.SetValue(name, value, RegistryValueKind.DWord);
+        }
+    }
+}
